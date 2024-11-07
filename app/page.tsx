854c375f977e3fc7a032826848483715897370af
Pankaj <pankajkumar.devone@gmail.com>
2024-11-07 21:00:10 +0530

    added analytics

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,11 @@ import Hero from "@/components/hero";
 import Appbar from "../components/appbar";
 import Content from "@/components/content";
 import Footer from "@/components/footer";
-
+import { Analytics } from "@vercel/analytics/react"
 export default function Home() {
   return (
     <div className="bg-gradient-to-b from-yellow-400 via-orange-500 to-yellow-600 min-h-screen">
+    <Analytics/>
     <Appbar/>
     <Hero/>
     <Content/>
